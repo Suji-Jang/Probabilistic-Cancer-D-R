7e632fd40f8dfe2b7e3f3768bc0d5b2cf6684525
Suji-Jang <54675037+Suji-Jang@users.noreply.github.com>
2022-11-01 04:26:06 -0500

    New folders

--- codes/Figure 2 - BMDL Comparison_GitHub.R
+++ codes/Figure 2 - BMDL Comparison_GitHub.R
@@ -1,4 +1,3 @@
-##### Comparison BMDL10 (MA, HW, BMDS) #####
 library(reshape2)
 library(ggpubr)
 library(viridis)
@@ -16,7 +15,7 @@ bmds.df <- fread(file.path(bmdfolder,"BMDS_final_data_092822.csv"))
 total.bmds.df <- fread(file.path(bmdfolder,"BMDS_RSD_092822.csv"))
 ma.df <- fread(file.path(bmdfolder,"MA_RSD_092822.csv"))
 hw.df <- fread(file.path(bmdfolder,"HW_RSD_092822.csv"))
-resultsfolder <- "results"
+figuresfolder <- "figures"
 
 errorbar.df <- data.frame(Index=1:255,MA.BMDL=ma.df[,"BMDL.10"],MA.BMD=ma.df[,"BMD.10"],MA.BMDU=ma.df[,"BMDU.10"],
                           HW.BMDL=hw.df[,"BMDL.10"],BMDS.BMDL=bmds.df[,"BMDL"])
@@ -49,7 +48,6 @@ p1 <- ggplot(errorbar.plot.df) +
         panel.border = element_rect(colour = "black",fill=NA), 
         plot.tag = element_text(size=20, face="bold"),plot.tag.position = c(0.1, 0.95))
   
-  
 hist.df <- data.frame(Index=1:255,MA.BMDL=ma.df[,"BMDL.10"],HW.BMDL=hw.df[,"BMDL.10"],
                       BMDS.BMDL=bmds.df[,"BMDL"])
 colnames(hist.df) <- c("Index","MA.BMDL","HW.BMDL","BMDS.BMDL")
@@ -77,13 +75,11 @@ p3 <- ggplot(hist.df,aes(x=BMDS.MA)) + geom_histogram() + labs(tag="C") +
         panel.border = element_rect(colour = "black",fill=NA), 
         plot.tag = element_text(size=20, face="bold"),plot.tag.position = c(0.07, 0.95))
 
-
-
 hist.scatter <- ggarrange(p2,p3,heights=c(5,5),widths=c(5,5)) # SIZE = 5 * 10
 sum.plot <- ggarrange(p1,hist.scatter,heights=c(6,4),widths=c(10,10),ncol=1)
-ggsave(file.path(resultsfolder,"Figure 2 - BMDL_100722.pdf"),height=10,width=8)
+ggsave(file.path(figuresfolder,"Figure 2 - BMDL_100722.pdf"),height=10,width=8)
 
-# For HW (Supplemental Fig 2) - HW vs. MA
+# For HW (Supplemental Fig) - HW vs. MA
 errorbar.hw.plot.df <- errorbar.df[order(errorbar.df$MA.BMD,errorbar.df$Index),]
 errorbar.hw.plot.df$order.index <- 1:255
 errorbar.hw.plot.df <- errorbar.hw.plot.df[,c("order.index","MA.BMD","HW.BMDL")]
@@ -107,24 +103,27 @@ p1 <- ggplot(errorbar.hw.plot.df) +
         panel.border = element_rect(colour = "black",fill=NA), 
         plot.tag = element_text(size=20, face="bold"),plot.tag.position = c(0.1, 0.95))
 
-
-p2 <- ggplot(hist.df,aes(x=HW.MA)) + geom_histogram() + 
-  scale_x_log10(limits=c(min(hist.df$HW.MA),max(hist.df$HW.MA)),breaks=10^c(-4,-3,-2,-1,0,1,2,3,4),   
-                labels=trans_format("log10",math_format(10^.x))) +
-  theme_classic() + xlab("HW/MA")
-
-p3 <- ggplot(hist.df,aes(x=MA.BMDL,y=HW.BMDL)) + geom_point() +
-  geom_abline() +
+p2 <- ggplot(hist.df,aes(x=MA.BMDL,y=HW.BMDL)) + geom_point() +
+  geom_abline() + labs(tag="B") +
   scale_x_log10(limits=c(min(errorbar.df[complete.cases(errorbar.df),][,c("HW.BMDL","BMDS.BMDL")])
                          ,max(errorbar.df[complete.cases(errorbar.df),][,c("HW.BMDL","BMDS.BMDL")])),
-                breaks=10^c(-6,-5,-4,-3,-2,-1,0,1,2,3),
-                labels=trans_format("log10",math_format(10^.x))) +
+                breaks=10^(-6:3),labels=trans_format("log10",math_format(10^.x))) +
   scale_y_log10(limits=c(min(errorbar.df[complete.cases(errorbar.df),][,c("HW.BMDL","BMDS.BMDL")])
                          ,max(errorbar.df[complete.cases(errorbar.df),][,c("HW.BMDL","BMDS.BMDL")])),
-                breaks=10^c(-6,-5,-4,-3,-2,-1,0,1,2,3),
+                breaks=10^-(6:3),labels=trans_format("log10",math_format(10^.x))) +
+  xlab("BMDL [MA]") + ylab("BMDL [HW]") + theme_classic() +
+  theme(panel.border = element_rect(colour = "black",fill=NA), 
+        plot.tag = element_text(size=20, face="bold"),plot.tag.position = c(0.2, 0.95))
+
+p3 <- ggplot(hist.df,aes(x=HW.MA)) + geom_histogram() + labs(tag="C") +
+  scale_x_log10(limits=c(min(hist.df$BMDS.MA),max(hist.df$BMDS.MA)),breaks=10^c(-4,-3,-2,-1,0,1,2,3,4),   
                 labels=trans_format("log10",math_format(10^.x))) +
-  theme_classic() 
+  xlab("BMDL [HW] / BMDL [MA]") +
+  theme_classic() + geom_vline(xintercept=1,linetype="dashed") +
+  theme(axis.text.y=element_blank(),axis.ticks.y=element_blank(),axis.title.y=element_blank(),
+        panel.border = element_rect(colour = "black",fill=NA), 
+        plot.tag = element_text(size=20, face="bold"),plot.tag.position = c(0.07, 0.95))
 
 hist.scatter <- ggarrange(p2,p3,heights=c(5,5),widths=c(5,5)) # SIZE = 5 * 10
 sum.plot <- ggarrange(p1,hist.scatter,heights=c(6,4),widths=c(10,10),ncol=1)
-ggsave(file.path(resultsfolder,"Supple Figure - BMDL_100722.pdf"),height=10,width=8)
+ggsave(file.path(figuresfolder,"Supple Figure - BMDL_100722.pdf"),height=10,width=8)
---
+++ codes/Figure 3 - RSD06 Comparison_GitHub.R
@@ -0,0 +1,104 @@
+library(reshape2)
+library(ggpubr)
+library(viridis)
+library(scales)
+library(data.table)
+
+functionfolder <- "functions"
+source(file.path(functionfolder,"Pop_incidence_functions.R"))
+source(file.path(functionfolder,"BMD_functions.R"))
+source(file.path(functionfolder,"HDMI_functions.R"))
+source(file.path(functionfolder,"Extra_risk_functions.R"))
+source(file.path(functionfolder,"RSD_functions.R"))
+bmdfolder <- "BMD-data"
+bmds.df <- fread(file.path(bmdfolder,"BMDS_final_data_092822.csv"))
+total.bmds.df <- fread(file.path(bmdfolder,"BMDS_RSD_092822.csv"))
+ma.df <- fread(file.path(bmdfolder,"MA_RSD_092822.csv"))
+hw.df <- fread(file.path(bmdfolder,"HW_RSD_092822.csv"))
+resultsfolder <- "results"
+rsd6.df <- fread(file.path(resultsfolder,"RSD06.quants.csv"))
+figuresfolder <- "figures"
+
+rsd6.df <- rsd6.df[,c("Dataset","X5.","X50.","X95.")]
+rsd6.df[,c("X5.","X50.","X95.")] <- 10^rsd6.df[,c("X5.","X50.","X95.")]
+colnames(rsd6.df) <- c("Index","Prob.5","Prob.50","Prob.95")
+rsd6.df$BMDS.RSD6 <- total.bmds.df$RSD.06
+rsd6.df$MA.RSD6 <- ma.df$RSD.06.L
+
+rsd6.df <- rsd6.df[order(rsd6.df$Prob.50,rsd6.df$Index),]
+rsd6.df$order.index <- 1:nrow(rsd6.df)
+
+rsd6.points.df <- rsd6.df[,c("order.index","MA.RSD6","BMDS.RSD6","Prob.50")]
+colnames(rsd6.points.df) <- c("order.index","BBMD-MA + Linear Extrapolation",
+                              "BMDS + Linear Extrapolation","BBMD-MA + Probabilistic Extapolation")
+rsd6.points.df <- melt(rsd6.points.df,id.vars="order.index")
+
+p1 <- ggplot(rsd6.points.df) +
+  geom_linerange(data=rsd6.df,mapping=aes(x=order.index,y=Prob.50,
+                                          ymin = Prob.95, ymax = Prob.5),alpha=0.5,colour="#6DCD59FF")+
+  geom_point(aes(x=order.index,y=value,colour=variable,shape=variable),alpha=0.7) +
+  scale_y_log10(limits=c(min(rsd6.df[,c("MA.RSD6","BMDS.RSD6","Prob.5")])
+                         ,max(rsd6.df[,c("MA.RSD6","BMDS.RSD6","Prob.95")])),
+                breaks=10^c(-11:2),labels=trans_format("log10",math_format(10^.x))) +
+  annotation_logticks(sides="l") +
+  ylab("RSD6") + labs(tag="A") +
+  scale_color_viridis(discrete=TRUE,end=0.8) +
+  theme_classic() +
+  theme(axis.text.x=element_blank(),axis.ticks.x=element_blank(),axis.title.x=element_blank(),
+        legend.title=element_blank(),legend.position = c(0.5, 0.1),
+        panel.border = element_rect(colour = "black",fill=NA), 
+        plot.tag = element_text(size=20, face="bold"),plot.tag.position = c(0.1, 0.95))
+
+hist.RSD6.df <- data.frame(Index=1:nrow(rsd6.df),Fit.RSD6=rsd6.df[,"Prob.5"],MA.RSD6=ma.df[,"RSD.06.L"],
+                           BMDS.RSD6=rsd6.df[,"BMDS.RSD6"])
+colnames(hist.RSD6.df) <- c("Index","Fit.RSD6","MA.RSD6","BMDS.RSD6")
+hist.RSD6.df$MA.Prob <- hist.RSD6.df$MA.RSD6/hist.RSD6.df$Fit.RSD6
+hist.RSD6.df$BMDS.Prob <- hist.RSD6.df$BMDS.RSD6/hist.RSD6.df$Fit.RSD6
+
+p2 <- ggplot(hist.RSD6.df,aes(x=Fit.RSD6,y=MA.RSD6)) + geom_point() +
+  geom_abline() + labs(tag="B") +
+  scale_x_log10(limits=c(min(hist.RSD6.df[complete.cases(hist.RSD6.df),][,c("Fit.RSD6","MA.RSD6","BMDS.RSD6")])
+                         ,max(hist.RSD6.df[complete.cases(hist.RSD6.df),][,c("Fit.RSD6","MA.RSD6","BMDS.RSD6")])),
+                breaks=10^c(-11:-1),labels=trans_format("log10",math_format(10^.x))) +
+  scale_y_log10(limits=c(min(hist.RSD6.df[complete.cases(hist.RSD6.df),][,c("Fit.RSD6","MA.RSD6","BMDS.RSD6")])
+                         ,max(hist.RSD6.df[complete.cases(hist.RSD6.df),][,c("Fit.RSD6","MA.RSD6","BMDS.RSD6")])),
+                breaks=10^c(-11:-1),labels=trans_format("log10",math_format(10^.x))) + 
+  xlab("RSD6 [BBMD-MA + Prob. Extr. (5th Perc.)]") + ylab("RSD6 [BBMD-MA + Linear Extr.]") + theme_classic() +
+  theme(panel.border = element_rect(colour = "black",fill=NA), 
+        plot.tag = element_text(size=20, face="bold"),plot.tag.position = c(0.2, 0.95))
+p3 <- ggplot(hist.RSD6.df,aes(x=Fit.RSD6,y=BMDS.RSD6)) + geom_point() +
+  geom_abline() + labs(tag="C") +
+  scale_x_log10(limits=c(min(hist.RSD6.df[complete.cases(hist.RSD6.df),][,c("Fit.RSD6","MA.RSD6","BMDS.RSD6")])
+                         ,max(hist.RSD6.df[complete.cases(hist.RSD6.df),][,c("Fit.RSD6","MA.RSD6","BMDS.RSD6")])),
+                breaks=10^c(-11:-1),labels=trans_format("log10",math_format(10^.x))) +
+  scale_y_log10(limits=c(min(hist.RSD6.df[complete.cases(hist.RSD6.df),][,c("Fit.RSD6","MA.RSD6","BMDS.RSD6")])
+                         ,max(hist.RSD6.df[complete.cases(hist.RSD6.df),][,c("Fit.RSD6","MA.RSD6","BMDS.RSD6")])),
+                breaks=10^c(-11:-1),labels=trans_format("log10",math_format(10^.x))) + 
+  xlab("RSD6 [BBMD-MA + Prob. Extr. (5th Perc.)]") + ylab("RSD6 [BMDS + Linear Extr.]") + theme_classic() +
+  theme(panel.border = element_rect(colour = "black",fill=NA), 
+        plot.tag = element_text(size=20, face="bold"),plot.tag.position = c(0.2, 0.95))
+
+p4 <- ggplot(hist.RSD6.df,aes(x=MA.Prob)) + geom_histogram() + labs(tag="D") +
+  scale_x_log10(limits=c(min(hist.RSD6.df[complete.cases(hist.RSD6.df),][,c("MA.Prob","BMDS.Prob")]),
+                         max(hist.RSD6.df[complete.cases(hist.RSD6.df),][,c("MA.Prob","BMDS.Prob")])),
+                breaks=10^c(-3:5),labels=trans_format("log10",math_format(10^.x))) + 
+  xlab(expression(frac("RSD6 [BBMD-MA + Linear Extr.]","RSD6 [BBMD-MA + Prob. Extr. (5th Perc.)]"))) +
+  theme_classic() + geom_vline(xintercept=1,linetype="dashed") +
+  theme(axis.text.y=element_blank(),axis.ticks.y=element_blank(),axis.title.y=element_blank(),
+        panel.border = element_rect(colour = "black",fill=NA), 
+        plot.tag = element_text(size=20, face="bold"),plot.tag.position = c(0.07, 0.95))
+
+p5 <- ggplot(hist.RSD6.df,aes(x=BMDS.Prob)) + geom_histogram() + labs(tag="E") +
+  scale_x_log10(limits=c(min(hist.RSD6.df[complete.cases(hist.RSD6.df),][,c("MA.Prob","BMDS.Prob")])
+                         ,max(hist.RSD6.df[complete.cases(hist.RSD6.df),][,c("MA.Prob","BMDS.Prob")])),
+                breaks=10^c(-3:5),labels=trans_format("log10",math_format(10^.x))) +
+  xlab(expression(frac("RSD6 [BMDS + Linear Extr.]","RSD6 [BBMD-MA + Prob. Extr. (5th Perc.)]"))) +
+  theme_classic() + geom_vline(xintercept=1,linetype="dashed") +
+  theme(axis.text.y=element_blank(),axis.ticks.y=element_blank(),axis.title.y=element_blank(),
+        panel.border = element_rect(colour = "black",fill=NA), 
+        plot.tag = element_text(size=20, face="bold"),plot.tag.position = c(0.07, 0.95))
+
+plot.2 <- ggarrange(p2,p3)
+plot.3 <- ggarrange(p4,p5)
+sum.plot <- ggarrange(p1,plot.2,plot.3,heights=c(6,4,4),widths=c(10,10),ncol=1)
+ggsave(file.path(figuresfolder,"Figure 3- RSD_100722.pdf"),height=12,width=8)
