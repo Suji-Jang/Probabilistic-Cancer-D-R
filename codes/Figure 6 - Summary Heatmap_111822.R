0ce3f2d6a435b414fa6c3a7ef64af38f2e4d7640
Suji-Jang <54675037+Suji-Jang@users.noreply.github.com>
2022-11-30 08:25:10 -0600

    Edited Heatmap

---
+++ codes/Figure 6 - Summary Heatmap_111822.R
@@ -0,0 +1,100 @@
+library(ggplot2)
+library(gplots)
+library(RColorBrewer)
+library(viridis)
+library(lsr)
+library(data.table)
+library(GGally)
+library(ggcorrplot)
+
+bmdfolder <- "BMD-data"
+weight.df <- fread(file.path(bmdfolder,"Model_weight_092022.csv"))
+bmds.df <- fread(file.path(bmdfolder,"BMDS_final_data_092822.csv"))
+ma.df <- fread(file.path(bmdfolder,"MA_RSD_092822.csv"))
+resultsfolder <- "results"
+rsd6.df <- fread(file.path(resultsfolder,"RSD06.quants.csv"))
+figuresfolder <- "figures"
+
+bmdl.df <- data.frame(Index=1:255,MA.BMDL=ma.df[,"BMDL.10"],MA.BMDU=ma.df[,"BMDU.10"],BMDS.BMDL=bmds.df[,"BMDL"])
+colnames(bmdl.df) <- c("Index","MA.BMDL","MA.BMDU","BMDS.BMDL")
+
+rsd6.df <- rsd6.df[,c("Dataset","X5.","X50.","X95.")]
+rsd6.df[,2:4] <- 10^rsd6.df[,2:4]
+rsd6.df$MA.RSD6 <- ma.df$RSD.06.L
+colnames(rsd6.df) <- c("Index","RSD6.Prob.05","RSD6.Prob.50","RSD6.Prob.95","RSD6.MA.Linear")
+
+heatmap.df <- weight.df[,2:9]
+heatmap.df$row.sum <- rowSums(heatmap.df)
+heatmap.df[,1:8] <- weight.df[,2:9]/heatmap.df$row.sum
+heatmap.df <- heatmap.df[,1:8]
+colnames(heatmap.df) <- c("QL","Log","Pro","Wei","MS2","LLog","LPro","DH")
+
+BBMD.BMDS.Ratio <- log10(bmdl.df$BMDS.BMDL/bmdl.df$MA.BMDL)
+heatmap.df$BBMD.BMDS.Ratio <- BBMD.BMDS.Ratio
+BMD.CI.Ratio <- log10(bmdl.df$MA.BMDU/bmdl.df$MA.BMDL)
+heatmap.df$BMD.CI.Ratio <- BMD.CI.Ratio
+Prob.Linear.Ratio <- log10(rsd6.df$RSD6.MA.Linear/rsd6.df$RSD6.Prob.05)
+heatmap.df$Prob.Linear.Ratio <- Prob.Linear.Ratio
+RSD.CI.Ratio <- log10(rsd6.df$RSD6.Prob.95/rsd6.df$RSD6.Prob.05)
+heatmap.df$RSD.CI.Ratio <- RSD.CI.Ratio
+
+# All parameters
+scattermat <- ggpairs(heatmap.df)
+ggsave(file.path(figuresfolder,"Figure 6 - Scatter.pdf"),scattermat,height=5,width=5,scale=2)
+corrmat <- ggcorrplot(round(cor(heatmap.df),2), type = "lower", show.diag = TRUE,
+                      lab = TRUE,)
+ggsave(file.path(figuresfolder,"Figure 6 - Corr.pdf"),corrmat,height=5,width=5,scale=1.5)
+
+# Ratio Only
+ratio.heatmap.df <- heatmap.df[,c("BBMD.BMDS.Ratio","BMD.CI.Ratio","Prob.Linear.Ratio","RSD.CI.Ratio")]
+scattermat <- ggpairs(ratio.heatmap.df)
+ggsave(file.path(figuresfolder,"Figure 6 - Scatter_Ratio.pdf"),scattermat,height=5,width=5,scale=2)
+corrmat <- ggcorrplot(round(cor(ratio.heatmap.df),2), type = "lower", show.diag = TRUE,
+                      lab = TRUE,)
+ggsave(file.path(figuresfolder,"Figure 6 - Corr_Ratio.pdf"),corrmat,height=5,width=5,scale=1.5)
+
+# Rescale 0 to 1 for heatmap
+heatmap.df$BBMD.BMDS.Ratio <- (BBMD.BMDS.Ratio-min(BBMD.BMDS.Ratio))/(max(BBMD.BMDS.Ratio)-min(BBMD.BMDS.Ratio))
+heatmap.df$BMD.CI.Ratio <- (BMD.CI.Ratio-min(BMD.CI.Ratio))/(max(BMD.CI.Ratio)-min(BMD.CI.Ratio))
+heatmap.df$Prob.Linear.Ratio <- (Prob.Linear.Ratio-min(Prob.Linear.Ratio))/(max(Prob.Linear.Ratio)-min(Prob.Linear.Ratio))
+heatmap.df$RSD.CI.Ratio <- (RSD.CI.Ratio-min(RSD.CI.Ratio))/(max(RSD.CI.Ratio)-min(RSD.CI.Ratio))
+
+mypalette <- (colorRampPalette(brewer.pal(9, "Reds"))(100))
+
+heatmap.data <- t(heatmap.df)
+pdf(file.path(figuresfolder,"Figure 6 - Heatmap - All.pdf"),height=6,width=20)
+heatmap.2(heatmap.data,col=mypalette, trace="none",density.info = "density",
+          Rowv=FALSE,dendrogram="column",lwid=c(0.8,8),lhei=c(1.5,5),margins=c(2,10))
+dev.off()
+
+heatmap.data.BMDL <- t(heatmap.df)[c(1:8,9,10),]
+pdf(file.path(figuresfolder,"Figure 6 - Heatmap - BMDL.pdf"),height=6,width=20)
+heatmap.2(heatmap.data.BMDL,col=mypalette, trace="none",density.info = "density",
+          Rowv=FALSE,dendrogram="column",lwid=c(0.8,8),lhei=c(1.5,5),margins=c(2,10))
+dev.off()
+
+heatmap.data.RSD <- t(heatmap.df)[c(1:8,11,12),]
+pdf(file.path(figuresfolder,"Figure 6 - Heatmap - RSD.pdf"),height=6,width=20)
+heatmap.2(heatmap.data.RSD,col=mypalette, trace="none",density.info = "density",
+          Rowv=FALSE,dendrogram="column",lwid=c(0.8,8),lhei=c(1.5,5),margins=c(2,10))
+dev.off()
+
+heatmap.data.Ratio <- t(heatmap.df)[c(9:12),]
+pdf(file.path(figuresfolder,"Figure 6 - Heatmap - Ratio.pdf"),height=4,width=20)
+heatmap.2(heatmap.data.Ratio,col=mypalette, trace="none",density.info = "density",
+          Rowv=FALSE,dendrogram="column",lwid=c(0.8,8),lhei=c(0.4,0.6),margins=c(2,10),cexRow=1)
+dev.off()
+
+# ANOVA
+bmdl.summary = lm(BMDL.Ratio ~ QL + Log + Pro + Wei + MS2 + LLog + LPro + DH - 1, data = heatmap.df)
+rsd.summary = lm(RSD.Ratio ~ QL + Log + Pro + Wei + MS2 + LLog + LPro + DH - 1, data = heatmap.df)
+
+aovbmdl <- aov(bmdl.summary)
+print(summary(bmdl.summary))
+print(summary(aovbmdl)) 
+print(etaSquared(aovbmdl))
+
+aovrsd <- aov(rsd.summary)
+print(summary(rsd.summary))
+print(summary(aovrsd)) 
+print(etaSquared(aovrsd))
